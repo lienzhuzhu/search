a2919c6ecae43658cee4687ed6b0137f0daf4593
Lien Zhu <lienzhucs@gmail.com>
2023-09-01 14:39:19 -0700

    fixes multiple start and goal cells

--- pathfinder/main.cpp
+++ pathfinder/main.cpp
@@ -15,6 +15,11 @@ int main()
     sf::Clock clock;
     float delta_time = 0.0f;
 
+    Coords start, goal;
+    Coords prev_start, prev_goal;
+    bool prev_start_set = false;
+    bool prev_goal_set = false;
+
     while (window.isOpen())
     {
         delta_time = clock.restart().asSeconds();
@@ -32,18 +37,30 @@ int main()
             }
         }
 
-        Coords start;
-        Coords goal;
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) || sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
             if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                // Step 1: Check if this is not first setting
+                if (prev_start_set) {
+                    map[prev_start.second][prev_start.first].setFillColor(GRAY);
+                }
+                // Step 2: Get new start.
                 start = get_mouse_cell(window);
-                std::cout << "start: " << start.first << " " << start.second << std::endl;
+                
+                // Step 3: Set new start cell to red.
                 map[start.second][start.first].setFillColor(RED);
+                
+                // Step 4: Update prev_start.
+                prev_start = start;
+                prev_start_set = true;
             }
             else {
+                if (prev_goal_set) {
+                    map[prev_goal.second][prev_goal.first].setFillColor(GRAY);
+                }
                 goal = get_mouse_cell(window);
-                std::cout << "goal: " << goal.first << " " << goal.second << std::endl;
                 map[goal.second][goal.first].setFillColor(YELLOW);
+                prev_goal = goal;
+                prev_goal_set = true;
             }
         }
 
